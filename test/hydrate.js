e83aff02e4adcc0e602626bea167e980e02ad91b
Neamar <neamar@neamar.fr>
2013-09-02 16:26:21 +0200

    Merge html and raw with metas

--- test/hydrate.js
+++ test/hydrate.js
@@ -10,29 +10,27 @@ var config = require('../config/configuration.js');
 
 describe('POST /hydrate API endpoint', function() {
   it('should refuse every request without a file', function(done) {
-    var req = request(app).post('/hydrate')
+    request(app).post('/hydrate')
       .expect(405)
       .end(done);
   });
 
   it('should accept request with a file attached', function(done) {
-    var req = request(app).post('/hydrate');
-
-    req.attach('file', __filename)
-        .expect(200)
-        .end(done);
+    
+    request(app).post('/hydrate')
+      .attach('file', __filename)
+      .expect(200)
+      .end(done);
   });
 
   it('should respond with the correct informations', function(done) {
-    var req = request(app).post('/hydrate');
-
-    req.attach('file', __filename)
+    request(app).post('/hydrate')
+      .attach('file', __filename)
       .expect('Content-Type', /json/)
       .end(function(err, res, body) {
       body.should.have.property('raw');
       body.should.have.property('html');
-      body.should.have.property('meta');
-      body.meta.should.have.property('created');
+      body.should.have.property('created');
 
       done();
     });
