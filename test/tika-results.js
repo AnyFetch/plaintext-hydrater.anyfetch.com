a523f2c3f700c39f1302c60e1e8d38de8a207f62
Neamar <neamar@neamar.fr>
2013-09-10 17:32:02 +0200

    Fake server does not handle JSON.

---
+++ test/tika-results.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var request = require('supertest');
+var should = require('should');
+var async = require('async');
+var http = require('http');
+var fs = require('fs');
+
+var app = require('../app.js');
+var config = require('../config/configuration.js');
+
+describe('Test tika results', function() {
+
+
+  it('returns the correct informations', function(done) {
+    // Create a fake HTTP server
+    http.createServer(function (req, res) {
+      if(req.url == "/file") {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        return res.end(fs.readFileSync(__filename))
+      } else {
+        //console.log(req);
+        req.params.metadatas.should.have.property('raw');
+        req.params.metadatas.should.have.property('html');
+        req.params.metadatas.should.have.property('content-encoding');
+        
+        done();
+      }
+    }).listen(1337, '127.0.0.1');
+
+    request(app).post('/hydrate')
+      .send({
+        metadatas: {},
+        file_path: 'http://127.0.0.1:1337/file',
+        callback: 'http://127.0.0.1:1337/result'
+      })
+      .expect(204)
+      .end(function() {});
+  });
+});
