3e901eb39036b225c41e5d6e5521a83079cd11ec
Robin Ricard <ricard.robin@gmail.com>
2013-11-29 17:42:13 +0100

    Defaults and stuff

--- .profile.d/tika.sh
+++ .profile.d/tika.sh
@@ -3,12 +3,13 @@
 echo "-----> Install Apache Tika"
 
 # Set tika vars
-export TIKA_VERSION="1.4"
-export TIKA_PATH="$HOME/vendor/bin/tika-$TIKA_VERSION.jar"
-export TIKA_URL="http://repo1.maven.org/maven2/org/apache/tika/tika-app/$TIKA_VERSION/tika-app-$TIKA_VERSION.jar"
-export TIKA_CONCURRENCY=2
+TIKA_VERSION=${TIKA_VERSION:"1.4"}
+TIKA_DIR=${TIKA_DIR:"$HOME/vendor/bin"}
+TIKA_PATH=${TIKA_PATH:"$TIKA_DIR/tika-$TIKA_VERSION.jar"}
+TIKA_URL=${TIKA_URL:"http://repo1.maven.org/maven2/org/apache/tika/tika-app/$TIKA_VERSION/tika-app-$TIKA_VERSION.jar"}
+TIKA_CONCURRENCY=${TIKA_CONCURRENCY:2}
 
 # Download tika
 echo "... to TIKA_PATH=$TIKA_PATH"
-mkdir -p $HOME/vendor/bin
+mkdir -p $TIKA_DIR
 curl $TIKA_URL > $TIKA_PATH &2> /dev/null
