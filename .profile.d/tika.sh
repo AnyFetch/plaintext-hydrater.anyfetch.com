f105ac258e48145a55bc7ea3fee298b5fad1e32d
Robin Ricard <ricard.robin@gmail.com>
2013-11-29 17:59:28 +0100

    Fuck my own logic

--- .profile.d/tika.sh
+++ .profile.d/tika.sh
@@ -8,9 +8,8 @@ TIKA_DIR=${TIKA_DIR:-"$HOME/vendor/bin"}
 TIKA_PATH=${TIKA_PATH:-"$TIKA_DIR/tika-$TIKA_VERSION.jar"}
 TIKA_URL=${TIKA_URL:-"http://repo1.maven.org/maven2/org/apache/tika/tika-app/$TIKA_VERSION/tika-app-$TIKA_VERSION.jar"}
 TIKA_CONCURRENCY=${TIKA_CONCURRENCY:-2}
-SUDO=${SUDO:-}
 
 # Download tika
 echo "... to TIKA_PATH=$TIKA_PATH"
-`$SUDO mkdir -p $TIKA_DIR`
-`$SUDO curl $TIKA_URL > $TIKA_PATH &2> /dev/null`
+mkdir -p $TIKA_DIR
+curl $TIKA_URL > $TIKA_PATH &2> /dev/null
