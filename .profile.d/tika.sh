c207dbc0a16a51b0cab7ae07f6181bde59f7bfd0
Robin Ricard <ricard.robin@gmail.com>
2013-11-29 18:04:12 +0100

    FUCKING -APP

--- .profile.d/tika.sh
+++ .profile.d/tika.sh
@@ -5,7 +5,7 @@ echo "-----> Install Apache Tika"
 # Set tika vars
 TIKA_VERSION=${TIKA_VERSION:-"1.4"}
 TIKA_DIR=${TIKA_DIR:-"$HOME/vendor/bin"}
-TIKA_PATH=${TIKA_PATH:-"$TIKA_DIR/tika-$TIKA_VERSION.jar"}
+TIKA_PATH=${TIKA_PATH:-"$TIKA_DIR/tika-app-$TIKA_VERSION.jar"}
 TIKA_URL=${TIKA_URL:-"http://repo1.maven.org/maven2/org/apache/tika/tika-app/$TIKA_VERSION/tika-app-$TIKA_VERSION.jar"}
 TIKA_CONCURRENCY=${TIKA_CONCURRENCY:-2}
 
