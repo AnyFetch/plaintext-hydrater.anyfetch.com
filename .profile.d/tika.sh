03a8aaab057a6cce04883c5e2ee1101aceaf6879
Neamar <neamar@neamar.fr>
2013-12-04 11:44:52 +0100

    Trying to simplify

--- .profile.d/tika.sh
+++ .profile.d/tika.sh
@@ -5,9 +5,9 @@ echo "-----> Install Apache Tika"
 # Set tika vars
 TIKA_VERSION=${TIKA_VERSION:-"1.4"}
 TIKA_DIR=${TIKA_DIR:-"$HOME/vendor/bin"}
-TIKA_PATH=${TIKA_PATH:-"$TIKA_DIR/tika-app-$TIKA_VERSION.jar"}
 TIKA_URL=${TIKA_URL:-"http://repo1.maven.org/maven2/org/apache/tika/tika-app/$TIKA_VERSION/tika-app-$TIKA_VERSION.jar"}
-TIKA_CONCURRENCY=${TIKA_CONCURRENCY:-2}
+export TIKA_PATH=${TIKA_PATH:-"$TIKA_DIR/tika-app-$TIKA_VERSION.jar"}
+export TIKA_CONCURRENCY=${TIKA_CONCURRENCY:-2}
 
 # Download tika
 echo "... to TIKA_PATH=$TIKA_PATH"
