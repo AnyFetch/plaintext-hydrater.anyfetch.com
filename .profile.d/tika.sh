be089e51c645b15844934518b0d8004e94428734
Neamar <neamar@neamar.fr>
2013-12-05 10:25:38 +0100

    Conditional tika install

--- .profile.d/tika.sh
+++ .profile.d/tika.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-echo "-----> Install Apache Tika"
 
 # Set tika vars
 TIKA_VERSION=${TIKA_VERSION:-"1.4"}
 TIKA_DIR=${TIKA_DIR:-"$HOME/vendor/bin"}
-TIKA_PATH=${TIKA_PATH:-"$TIKA_DIR/tika-app-$TIKA_VERSION.jar"}
-TIKA_URL=${TIKA_URL:-"http://repo1.maven.org/maven2/org/apache/tika/tika-app/$TIKA_VERSION/tika-app-$TIKA_VERSION.jar"}
-TIKA_CONCURRENCY=${TIKA_CONCURRENCY:-2}
 
-# Download tika
-echo "... to TIKA_PATH=$TIKA_PATH"
-mkdir -p $TIKA_DIR
-curl $TIKA_URL > $TIKA_PATH &2> /dev/null
+export TIKA_CONCURRENCY=${TIKA_CONCURRENCY:-2}
+export TIKA_PATH=${TIKA_PATH:-"$TIKA_DIR/tika-app-$TIKA_VERSION.jar"}
+
+if [ ! -f $TIKA_PATH ]; then
+  # Download tika
+  TIKA_URL=${TIKA_URL:-"http://repo1.maven.org/maven2/org/apache/tika/tika-app/$TIKA_VERSION/tika-app-$TIKA_VERSION.jar"}
+  echo "-----> Install Apache Tika to TIKA_PATH=$TIKA_PATH"
+  mkdir -p $TIKA_DIR
+  curl $TIKA_URL > $TIKA_PATH &2> /dev/null
+fi
